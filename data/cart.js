139f91e901846739c1cbad688ce94a5200a87fa2
Revanth_90 <revanthkolla90@gmail.com>
2025-08-17 12:35:19 +0530

    local storage

--- data/cart.js
+++ data/cart.js
@@ -1,14 +1,20 @@
-export let  cart =[{
+export let  cart = JSON.parse(localStorage.getItem('cart'));
+if(!cart)
+cart = [{
   productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity : 2,
 },{
     productId :'15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity : 1
 }];
+
+function saveToStorage(){
+    localStorage.setItem('cart',JSON.stringify(cart));
+}
 export function addtocart(productId){
     let matchingItem;
     cart.forEach((item)=>{
-        if(item.id === productId){
+        if(item.productId === productId){
             matchingItem = item;
         }
 
@@ -17,10 +23,11 @@ export function addtocart(productId){
         matchingItem.quantity++;
     }else{
     cart.push({
-        id: productId,
+        productId: productId,
         quantity: 1
     })
     }
+    saveToStorage();
 }
 
 export function deleteProduct(productId){
@@ -31,4 +38,5 @@ export function deleteProduct(productId){
         }
     })
     cart = newCart;
+    saveToStorage();
 }
